a45b1a3fdf1b4508787617ad0ebfa625992d999d
plowsof <plowsof@protonmail.com>
2025-11-05 21:09:13 +0000

    inotify: support 2 torrents

--- inotify.sh
+++ inotify.sh
@@ -9,28 +9,30 @@ inotifywait -m -r -e create --format '%w%f' "$WATCH_DIR" | while read -r NEWFILE
   echo "new file: $BASE_NAME"
   IS_MONERO=0
   case $BASE_NAME in
-    monero-v*.torrent)
+    monero-v*.torrent|monero-gui-v*.torrent)
     IS_MONERO=1
+    BASE_NAME="${BASE_NAME%.torrent}"
     ;;
   *)
     ;;
   esac
   if [ "$IS_MONERO" == 1 ]; then
     # https://stackoverflow.com/a/68140834
-    NEW_VER="${BASE_NAME#monero-v}"
-    NEW_VER="${NEW_VER%.torrent}"
+    PREFIX="monero-gui-v"
+    [[ $BASE_NAME != monero-gui-v* ]] && PREFIX="monero-v"
+    NEW_VER="${BASE_NAME#$PREFIX}"
 
     echo "New Monero version: $NEW_VER" # "0.18.4.3"
-    for OLD_DIR in "$DOWNLOAD_DIR"/monero-v*; do
-      OLD_VER="${OLD_DIR##*/monero-v}"
+    for OLD_DIR in "$DOWNLOAD_DIR/$PREFIX"*; do
+      OLD_VER="${OLD_DIR##*/$PREFIX}"
       if [ "$OLD_VER" != "$NEW_VER" ]; then
         echo "Deleting: $OLD_DIR"
         rm -rf "$OLD_DIR"
       fi
     done
 
-    for OLD_TORRENT in "$WATCH_DIR"/monero-v*.torrent; do
-      OLD_VER="${OLD_TORRENT##*/monero-v}"
+    for OLD_TORRENT in "$WATCH_DIR/$PREFIX"*.torrent; do
+      OLD_VER="${OLD_TORRENT##*/$PREFIX}"
       OLD_VER="${OLD_VER%.torrent}"
       if [ "$OLD_VER" != "$NEW_VER" ]; then
         echo "Deleting: $OLD_TORRENT"
